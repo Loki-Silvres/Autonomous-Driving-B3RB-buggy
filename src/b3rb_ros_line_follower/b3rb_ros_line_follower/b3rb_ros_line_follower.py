9cd5ae03134a2fa3f3323c88709868944934dc46
Aman3214 <aman48anand@gmail.com>
2024-08-09 18:16:06 +0530

    attempted obstacle avoidance

--- src/b3rb_ros_line_follower/b3rb_ros_line_follower/b3rb_ros_line_follower.py
+++ src/b3rb_ros_line_follower/b3rb_ros_line_follower/b3rb_ros_line_follower.py
@@ -45,13 +45,12 @@ SPEED_25_PERCENT = SPEED_MAX / 4
 SPEED_50_PERCENT = SPEED_25_PERCENT * 2
 SPEED_75_PERCENT = SPEED_25_PERCENT * 3
 
-THRESHOLD_OBSTACLE_VERTICAL = 1.0
-THRESHOLD_OBSTACLE_HORIZONTAL = 0.25
+THRESHOLD_OBSTACLE_VERTICAL = 0.7
+THRESHOLD_OBSTACLE_HORIZONTAL = 0.20
 THRESHOLD_RAMP = 0.5
 
 MIN_FWD_VEL = 0.4
 MAX_FWD_VEL = 1.0
-
 class LineFollower(Node):
 	""" Initializes line follower node with the required publishers and subscriptions.
 
@@ -115,7 +114,7 @@ class LineFollower(Node):
 		self.time_now = -100
 		# self.free_ranges = []/
 		#-----for Aman's code-----
-		self.value = 0
+		self.value = 0.0
 		# ------till here---------
 
 		self.my_vel_timer = self.create_timer(
@@ -232,14 +231,15 @@ class LineFollower(Node):
 			# speed = max(SPEED_MAX/4, speed)
 			speed = 0.6
 			# self.time_now = time.time()
-			print("ramp/bridge detected")
+			self.get_logger().info("ramp/bridge detected")
 			# self.get_logger().info("ramp/bridge detected")
 
 		if self.obstacle_detected is True:
 			# TODO: participants need to decide action on detection of obstacle.
 			# print("obstacle detected")
 			turn = self.value 
-			speed = max(0.1, 0.9 - min(abs(turn), 0.9))
+			# speed = max(0.1, 0.9 - min(abs(turn), 0.9))
+			speed = 0.5
 			self.get_logger().info(f"turn: {self.value:.3f}, speed: {speed:.3f}")
 		
 
@@ -291,118 +291,52 @@ class LineFollower(Node):
 		side_ranges_right = ranges[0: int(length * theta / PI)]
 		side_ranges_left = ranges[int(length * (PI - theta) / PI):]
 
-		# side_ranges_right = ranges[0: int(length / 2)]
-		# side_ranges_left = ranges[int(length / 2):]
-
-
-		# message.ranges = [i/100 for i in range(len(message.ranges))]
-		# self.publish_front_lidar.publish(message)
-
-
-		# polars =[]
-		# for i in range(len(message.ranges)):
-		# 	theta_ = i * resolution
-		# 	r = message.ranges[i]
-		# 	polar = [r, theta_]
-		# 	polars.append(polar)
-		
-		# self.get_logger().info(f'{int(length * theta / PI), int(length * (PI - theta) / PI)}')
-		# polars = polars[int(length / 4): int(3 * length / 4)]
-		# front_polars = polars[int(length * theta / PI): int(length * (PI - theta) / PI)]
-		# front_carts = [[front_polars[i][0] * math.cos(front_polars[i][1]), front_polars[i][0] * math.sin(front_polars[i][1])] for i in range(len(front_polars))]
-		# free_indices = [ i for i in range(len(front_carts)) if front_carts[i][1] > THRESHOLD_OBSTACLE_VERTICAL]		
-
-		# # self.free_ranges = 
-		# self.lidar_dev = np.mean(free_indices)
-		# self.get_logger().info(f"front_ranges:{front_ranges}\nfront_polars:{front_polars}\nfront_carts:{front_carts}")
-		# self.get_logger().info(f"\n\nmean:{self.lidar_dev}, free_indices:{free_indices}  \n\n")
-
-		# front_msg = message
-		# for i in range(int(length / 4)+ int(length * (PI - theta) / PI), len(front_msg.ranges)):
-		# 	front_msg.ranges[i] = 10.0
-
-		# for i in range(len(front_msg.ranges[:int(length / 4)+int(length * theta / PI)])):
-
-		# 	front_msg.ranges[i] = 10.0
-
-		# self.publish_front_lidar.publish(front_msg)
 		# process front ranges.
 		angle = theta - PI / 2
 		# --------------Aman's trial code----------------------------
-		value = 0
-		self.value = 0
+		value = 0.0
+		self.value = 0.0
 		self.obstacle_detected = False
+		self.ramp_detected = False
 		# for time when obstacle in front 
-		for i in range(len(front_ranges)):
-			if(front_ranges[i] < THRESHOLD_RAMP and front_ranges[i] > THRESHOLD_OBSTACLE_HORIZONTAL):
-				self.obstacle_detected = True
-				self.get_logger().info("obstacle detected")
-			else:
-				if(i<len(front_ranges)/2):
-					value += -self.norm_func(i)
-				else:
-					value += self.norm_func(i)
+		deg = int(len(front_ranges)/2)
+		for i in range(deg):
+			value += (front_ranges[i]-front_ranges[deg*2-i-1])/10
 			angle += message.angle_increment
+		value = value/deg
 
+		if(abs(value)>THRESHOLD_OBSTACLE_HORIZONTAL and abs(value)<THRESHOLD_OBSTACLE_VERTICAL):
+			self.obstacle_detected = True
+			self.get_logger().info("obstacle detected in front")
+			self.value = value
+			self.get_logger().info(f"value of turn: {value}")
+		elif(abs(value)<THRESHOLD_OBSTACLE_HORIZONTAL and abs(value)>0.07):
+			self.ramp_detected = True
+		self.get_logger().info(f"----------------------------------------")
+		# --------------alternate----------------------------
+		
+		# ---------------------------------------------------
+		# self.get_logger().info(f"value of turn: {value}")
 		#for the time when obstacles and no line
-		left_avg = 0
-		right_avg = 0	
-		for i in range(len(side_ranges_left)):
-			if(side_ranges_left[i]<THRESHOLD_OBSTACLE_HORIZONTAL or side_ranges_left[i]<THRESHOLD_OBSTACLE_HORIZONTAL):
-				self.get_logger().info("obstacle detected on left")
-				self.obstacle_detected = True
-				left_avg += side_ranges_left[i]
-			angle += message.angle_increment
-		for i in range(len(side_ranges_right)):
-			if(side_ranges_right[i]<THRESHOLD_OBSTACLE_HORIZONTAL or side_ranges_right[i]<THRESHOLD_OBSTACLE_HORIZONTAL):
-				self.get_logger().info("obstacle detected on right")
-				self.obstacle_detected = True
-				right_avg += side_ranges_right[i]
-			angle += message.angle_increment
-		# value = -value
-		if((left_avg-right_avg)!=0) :
-			value = (-left_avg+right_avg)
-		self.value = value/10
+		# left_avg = 0
+		# right_avg = 0	
+		# for i in range(len(side_ranges_left)):
+		# 	if(side_ranges_left[i]<THRESHOLD_OBSTACLE_HORIZONTAL or side_ranges_left[i]<THRESHOLD_OBSTACLE_HORIZONTAL):
+		# 		self.get_logger().info("obstacle detected on left")
+		# 		self.obstacle_detected = True
+		# 		left_avg += side_ranges_left[i]
+		# 	angle += message.angle_increment
+		# for i in range(len(side_ranges_right)):
+		# 	if(side_ranges_right[i]<THRESHOLD_OBSTACLE_HORIZONTAL or side_ranges_right[i]<THRESHOLD_OBSTACLE_HORIZONTAL):
+		# 		self.get_logger().info("obstacle detected on right")
+		# 		self.obstacle_detected = True
+		# 		right_avg += side_ranges_right[i]
+		# 	angle += message.angle_increment
+		# # value = -value
+		# if((left_avg-right_avg)!=0) :
+		# 	value = (-left_avg+right_avg)
 
 		#---------commment till here to remove Aman's code-------------
-
-		
-
-		# self.get_logger().info("length: {}".format(front))
-		# for i in range(len(front_ranges)):
-
-		# 	angle += message.angle_increment
-
-
-		# 	self.get_logger().info("max:{}, min:{},obstacle detected:{}".format(max, min,self.obstacle_detected))
-		# 	if (front_ranges[i] < THRESHOLD_OBSTACLE_VERTICAL):
-				
-		# 		self.obstacle_detected = True
-		# 		# return #TODO: check if this is needed
-		# 	else:
-
-		# 		if (i< self.min):
-		# 			self.min = i
-		# 		if (i> self.max):
-		# 			self.max = i
-		# 	# elif (front_ranges[i]< THRESHOLD_RAMP):
-		# 	# 	self.ramp_detected = True
-		# 	# 	self.get_logger().info("ramp/bridge detected")
-
-
-		# # process side ranges.
-		# side_ranges_left.reverse()
-		# for side_ranges in [side_ranges_left, side_ranges_right]:
-		# 	angle = 0.0
-		# 	for i in range(len(side_ranges)):
-		# 		if (side_ranges[i] < THRESHOLD_OBSTACLE_HORIZONTAL):
-		# 			self.obstacle_detected = True
-		# 			return
-
-		# 		angle += message.angle_increment
-
-		# self.obstacle_detected = False
-
 
 def main(args=None):
 	rclpy.init(args=args)
